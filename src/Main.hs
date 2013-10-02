ead37b2a6c492da87e5bfdd924bdb482beed68ea
Stephen Demos <stphndemos@gmail.com>
2013-10-01 22:16:09 -0400

    Moving dev over to fangorn
    don't even worry about this commit, nothing to see here...

--- src/Main.hs
+++ src/Main.hs
@@ -75,7 +75,7 @@ site =
                       ("users/:userName", listAllUserAchievements)
                     ]
         post = route [ ("users/:userName", createUser),
-                       ("apps/:appName/:userName/:achievementid/:achievementProgress", updateAchievement)
+                       ("apps/:appName/achievements", updateAchievement)
                      ]
 
 -- Should research if HDBC's SQL properly escapes everything if put in as parameters
@@ -149,12 +149,13 @@ getAppAchievements appName = do
   return $ jsonAssemble "achievements" ["id", "title", "description", "score"] rows
 
 getAchievement appName achievementid = do
-  rows <- getQuery "SELECT achievements.id, title, achievements.description, score FROM achievements INNER JOIN apps on achievements.app_id = apps.id WHERE achievements.id = (?) AND apps.name like (?)" [toSql achievementid, toSql appname]
+  rows <- getQuery "SELECT achievements.id, title, achievements.description, score FROM achievements INNER JOIN apps on achievements.app_id = apps.id WHERE achievements.id = (?) AND apps.name like (?)" [toSql achievementid, toSql appName]
   return $ jsonAssemble "achievements" ["id", "title", "description", "score"] rows
 
+getUserAchievementProgress :: [Char] -> [Char] -> Int -> IO [Integer]
 getUserAchievementProgress appName userName achievementid = do
-  progress <- map fromSql (getQuery "SELECT t1.progress, t2.progress_max FROM (achievement_progress AS t1 INNER JOIN achievements AS t2 ON t1.achievement_id=t2.id INNER JOIN apps on apps.id = t2.app_id) JOIN users AS t3 ON t1.user_id=t3.id WHERE t3.username=(?) AND apps.name like (?)" [toSql userName, toSql appName] !! 0)
-  
+  progressQuery <- (getQuery "SELECT t1.progress, t2.progress_max FROM (achievement_progress AS t1 INNER JOIN achievements AS t2 ON t1.achievement_id=t2.id INNER JOIN apps on apps.id = t2.app_id) JOIN users AS t3 ON t1.user_id=t3.id WHERE t3.username=(?) AND apps.name like (?)" [toSql userName, toSql appName])
+  return $ map fromSql $ progressQuery !! 0
 
 listUserAchievements :: Snap ()
 listUserAchievements = do
@@ -187,19 +188,20 @@ createUser :: Snap()
 createUser = do
   appName <- safeGetParam "appName"
   userName <- safeGetParam "userName"
+  writeBS "Not finished yet..."
 
 updateAchievement :: Snap ()
 updateAchievement = do
   appName <- safeGetParam "appName"
-  userName <- safeGetParam "userName"
-  achievementid <- safeGetParam "achievementid"
-  achievementProgress <- safeGetParam "achievementProgress"
+  userName <- safeGetParam "username"
+  achievementid <- safeGetParam "id"
+  achievementProgress <- safeGetParam "progress"
   currProg <- getUserAchievementProgress appName userName achievementid
-  writeText show updateProg appName userName achievementid achievementProgress currProg
+  writeText $ show $ updateProg appName userName achievementid achievementProgress currProg
 
 updateProg appName userName achievementid achievementProgress currProg
     | currProg !! 0 >= currProg !! 1 = "You've already earned this achievement!"
     | currProg !! 0 + achievementProgress >= currProg !! 1 = "You've earned this achievement, in real time!"
-    | currProg !! 0 < currProg !! 1 = "Old Progress - " ++ show $ currProg !! 0 ++ "\nNew Progress - " ++ show $ currProg !! 0 + achievementProgress
+    | currProg !! 0 < currProg !! 1 = "Old Progress - " ++ (currProg !! 0) ++ "\nNew Progress - " ++ (currProg !! 0) ++ achievementProgress
 
 --EOF--
